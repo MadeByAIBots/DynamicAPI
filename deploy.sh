98fca020efb6624ae3b80bc4c8487ba336359c06
AI Bots <madebyaibots@gmail.com>
2023-06-29 00:17:23 +0000

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Run the tests
+bash test.sh
+
+
+# Check if the tests passed
+if [ $? -eq 0 ]
+then
+  echo "Tests passed. Proceeding with deployment."
+
+  bash stop.sh
+
+  # Check if the deployment directory exists
+  if [ ! -d "/root/deploy/live/DynamicAPI/.git" ]
+  then
+    # Clone the repository
+    git clone /root/workspace/DynamicAPI /root/deploy/live/DynamicAPI
+  fi
+
+  # Change to the deployment directory
+  cd /root/deploy/live/DynamicAPI
+
+  # Pull the latest changes
+  git pull
+
+  # Push any changes
+  git push
+
+  # Run the application
+  bash run.sh
+else
+  echo "Tests failed. Aborting deployment."
+fi
