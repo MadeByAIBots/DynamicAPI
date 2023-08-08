b8eebf8697e47ef0d7a7f10af0ed88e5f2bd66d4
MadeByAIBots <madebyaibots@gmail.com>
2023-08-08 18:10:34 +1000

    Fixing config utils script to handle config override file not existing

--- config_utils.sh
+++ config_utils.sh
@@ -1,12 +1,15 @@
-# config_utils.sh
 get_config_value() {
     local key=$1
 
     override_file='config.override.json'
     default_file='config.json'
 
-    local value=$(jq -r ".${key}" ${override_file})
-    if [ "${value}" == "null" ]; then
+    local value
+    if [ -f "${override_file}" ]; then
+        value=$(jq -r ".${key}" ${override_file})
+    fi
+
+    if [ "${value}" == "null" ] || [ -z "${value}" ]; then
         value=$(jq -r ".${key}" ${default_file})
     fi
 
