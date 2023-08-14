7a8165cae8550c309e0090279677a4ed869711d8
MadeByAIBots <133623477+MadeByAIBots@users.noreply.github.com>
2023-08-14 21:45:10 +1000

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Install .NET 7
-wget https://dot.net/v1/dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 7.0 --install-dir /usr/share/dotnet
+#wget https://dot.net/v1/dotnet-install.sh
+#chmod +x dotnet-install.sh
+#./dotnet-install.sh -c 7.0 --install-dir /usr/share/dotnet
 
-# Install jq
 sudo apt-get update
-sudo apt-get install -y jq
+sudo apt-get install -y jq dotnet-sdk-7.0 python3 python3-pip python-is-python3
